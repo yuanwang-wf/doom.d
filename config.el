af8fed36efb412e1ca424bfbb285ef352ba8fc52
Yuan Wang <yuan.wang@workiva.com>
2020-03-28 19:22:59 -0600

    add basic dart, java, python support

--- config.el
+++ config.el
@@ -51,4 +51,19 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
+;;python setup
 (pyenv-mode)
+
+;;dart setup
+(setq lsp-dart-analysis-sdk-dir (getenv "DART_SDK"))
+(setq dart-format-on-save t)
+(add-hook 'dart-mode-hook 'lsp)
+(with-eval-after-load "projectile"
+  (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")
+  (add-to-list 'projectile-project-root-files-bottom-up "BUILD"))
+(setq lsp-auto-guess-root t)
+
+;;java setup
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
